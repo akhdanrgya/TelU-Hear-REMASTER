6fee16495bead9e5a4e8d3238cbada3a33de8622
akhdanrgya <akhdan.anargya@gmail.com>
2025-01-05 22:39:10 +0700

    Update Home

--- src/main/java/com/tubes/teluhear/HomeController.java
+++ src/main/java/com/tubes/teluhear/HomeController.java
@@ -1,4 +1,113 @@
 package com.tubes.teluhear;
 
-public class HomeController {
+import com.tubes.teluhear.database.PlaylistDAO;
+import com.tubes.teluhear.database.PlaylistModel;
+import com.tubes.teluhear.database.MusicDAO;
+import com.tubes.teluhear.database.MusicModel;
+import com.tubes.teluhear.database.dbConnection;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable {
+
+    @FXML
+    private GridPane musicGrid;
+
+    @FXML
+    private GridPane playlistGrid;
+
+    @FXML
+    private Label welcomeText;
+
+    private PlaylistDAO playlistDAO;
+    private MusicDAO musicDAO;
+
+    private int userId;
+
+
+    public void initialize(URL url, ResourceBundle rb) {
+        playlistDAO = new PlaylistDAO(dbConnection.getConnection());
+        musicDAO = new MusicDAO(dbConnection.getConnection());
+
+        List<PlaylistModel> playlistDataList = playlistDAO.getPlaylistByUser(userId);
+        List<MusicModel> musicDataList = musicDAO.getAllMusic();
+
+        populatePlaylist(playlistDataList);
+        populateMusic(musicDataList);
+
+    }
+
+    private void populatePlaylist(List<PlaylistModel> playlistModelList) {
+        if (playlistModelList == null || playlistModelList.isEmpty()) {
+            System.out.println("No playlist found");
+            return;
+        }
+
+        int column = 0;
+        int row = 0;
+
+        for (int i = 0; i < playlistModelList.size(); i++) {
+            PlaylistModel playlistModel = playlistModelList.get(i);
+
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/tubes/teluhear/PlaylistCard.fxml"));
+                Pane playlistCardView = loader.load();
+
+                PlaylistCardController playlistCardController = loader.getController();
+                playlistCardController.setPlaylistData(playlistModel);
+
+                playlistGrid.add(playlistCardView, column, row);
+                playlistGrid.setMargin(playlistCardView, new Insets(10));
+
+                column++;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void populateMusic(List<MusicModel> musicModelList) {
+        if (musicModelList == null || musicModelList.isEmpty()) {
+            System.out.println("No music found");
+            return;
+        }
+
+        int column = 0;
+        int row = 0;
+
+        for (int i = 0; i < musicModelList.size(); i++) {
+            MusicModel musicModel = musicModelList.get(i);
+
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/tubes/teluhear/MusicCard.fxml"));
+                Pane musicCardView = loader.load();
+
+                MusicCardController musicCardController = loader.getController();
+                musicCardController.setMusicData(musicModel, i + 1);
+
+                musicGrid.add(musicCardView, column, row);
+
+                row++;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
 }
+
+
