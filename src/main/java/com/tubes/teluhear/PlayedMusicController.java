2e946d0d4f57635b1b49269f4e6ca09b2a597b5c
akhdanrgya <akhdan.anargya@gmail.com>
2025-01-05 23:34:23 +0700

    Update Played Music

---
+++ src/main/java/com/tubes/teluhear/PlayedMusicController.java
@@ -0,0 +1,31 @@
+package com.tubes.teluhear;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+
+public class PlayedMusicController {
+
+    @FXML
+    private Label Judul;
+
+    @FXML
+    private Slider SliderMusic;
+
+    @FXML
+    void Next(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Play(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Prev(ActionEvent event) {
+
+    }
+
+}
